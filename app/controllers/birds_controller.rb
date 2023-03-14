d1b5156cfddc42581790ffd4eb85f454d737a2e5
Jesse <jessewambu@zetech.ac.ke>
2023-03-14 07:48:46 +0300

    Added Soln

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,12 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+  # POST /birds
+  def create
+    bird = Bird.create(params.permit(:name, :species))
+    render json: bird, status: :created
+  end
+
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -16,8 +22,4 @@ class BirdsController < ApplicationController
     end
   end
 
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
 end
